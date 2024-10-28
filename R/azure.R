b48b3927dda9e6cda4411bcabbcbbabd19317a40
Matt Dray <18232097+matt-dray@users.noreply.github.com>
2024-10-28 09:55:18 +0000

    Filter for run_stages before converting to factor

--- R/azure.R
+++ R/azure.R
@@ -92,7 +92,8 @@ fetch_tagged_runs_meta <- function(container_results, container_support) {
 
   result_sets <- get_nhp_result_sets(container_results, container_support)
 
-  # Factor levels to order run_stage by
+  # Run stages to keep/convert to factor levels (in order of preference for
+  # selection). This set may expand in future as we add more NDG variants.
   run_stages <- c(
     "final_report_ndg2",  # first level because it's preferred
     "final_report_ndg1",
@@ -103,11 +104,17 @@ fetch_tagged_runs_meta <- function(container_results, container_support) {
   )
 
   latest_tagged_runs <- result_sets |>
-    dplyr::filter(!is.na(run_stage)) |>
-    dplyr::select(dataset, scenario, create_datetime, run_stage, file) |>
+    dplyr::filter(run_stage %in% run_stages) |>
+    dplyr::select(dataset, scenario, run_stage, file) |>
     dplyr::mutate(run_stage = forcats::fct(run_stage, levels = run_stages)) |>
     dplyr::arrange(dataset, run_stage) |>  # run_stage will be ordered by level
-    dplyr::slice(1, .by = dataset)  # isolates the 'top' level within a scheme
+    dplyr::slice(1, .by = dataset) |>  # isolates the 'top' level within a scheme
+    dplyr::mutate(
+      run_stage = run_stage |>
+        as.character() |>  # convert from factor to allow string handling
+        stringr::str_remove("(_report)?_ndg\\d") |>
+        stringr::str_to_sentence()  # 'Initial', 'Intermediate', 'Final'
+    )
 
 }
 
